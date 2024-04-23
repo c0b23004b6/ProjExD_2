28dfc71612156827a7e39ca3caab74ab7db4f0d3
Asahi <c0b23004b6@edu.teu.ac.jp>
2024-04-23 15:18:44 +0900

    練習4:衝突判定

--- ex2-20240423/dodge_bomb.py
+++ ex2-20240423/dodge_bomb.py
@@ -50,6 +50,8 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bd_rct): # こうかとんと爆弾がぶつかったとき
+            return
         screen.blit(bg_img, [0, 0]) 
 
         # こうかとんの移動と表示
